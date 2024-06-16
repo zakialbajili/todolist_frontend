a1a2f41709e1baea58309347ca7f0aad64c50c53
zakyalbajili10 <zakyalbajili10@gmail.com>
2024-06-16 04:33:01 +0000

    move to local from idx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <Navbar/>
+    //tesss
 
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
